e77ced69631a8a4f9b1890982c66392e842508fd
Mario vizcaino <mario.vizcainos@gmail.com>
2017-11-25 22:27:22 -0500

    setup the view to chose the drink type

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,17 +1,57 @@
 <template>
-
+  <div class="layout text-center">
+    <h3>Chose a Drink Type</h3>
+    <div class="btn-list">
+      <div class="btn-container">
+        <router-link :to="{path: 'drinklist/Alcoholic'}">
+          <q-btn class="bg-primary">
+            Alcoholic
+          </q-btn>
+        </router-link>
+      </div>
+      <div class="btn-container">
+        <router-link :to="{path: 'drinklist/Non_Alcoholic'}">
+          <q-btn  class="bg-secondary">
+            Non Alcoholic
+          </q-btn>
+        </router-link>
+      </div>
+      <div class="btn-container">
+        <router-link :to="{path: 'drinkdetails/random'}">
+          <q-btn class="bg-tertiary">
+            Random
+          </q-btn>
+        </router-link>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import {QBtn} from 'quasar'
 export default {
   name: 'index',
   data () {
     return {
     }
+  },
+  components: {
+    QBtn
   }
 }
 </script>
 
-<style lang="stylus">
-
+<style scoped>
+  .layout{
+    margin-top: 50px;
+  }
+  .btn-list{
+    margin-top: 50px;
+  }
+  .btn-container{
+    margin: 20px;
+  }
+  .btn-container button{
+    color: #fff;
+  }
 </style>
