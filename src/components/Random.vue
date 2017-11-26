471c2ee27c579739f482a2473ae1b6c067ec740c
Mario vizcaino <mario.vizcainos@gmail.com>
2017-11-25 23:17:11 -0500

    list the random drink

--- src/components/Random.vue
+++ src/components/Random.vue
@@ -1,8 +1,28 @@
 <template>
-  
+  <div>
+    {{drink}}
+  </div>
 </template>
 <script>
 export default {
-  
+  data () {
+    return {
+      drink: {}
+    }
+  },
+  methods: {
+    fetchItems () {
+      this.items = []
+      this.type = this.$route.params.type
+      fetch('http://www.thecocktaildb.com/api/json/v1/1/random.php', {
+        method: 'GET'
+      })
+        .then(response => response.json())
+        .then(json => (this.drink = json.drinks))
+    }
+  },
+  created () {
+    this.fetchItems()
+  }
 }
 </script>
