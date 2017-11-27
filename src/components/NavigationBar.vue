46a5c4a2b1489839a324279238103e8ebf963b1e
Mario vizcaino <mario.vizcainos@gmail.com>
2017-11-26 22:22:58 -0500

    created navigation bar component

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <q-toolbar slot="header">
+      <q-btn 
+        flat 
+        @click="goBack"
+      >
+        <q-icon v-if="canGoBack" name="back" />
+      </q-btn>
+    <q-toolbar-title to="/" class="text-center" >
+    {{title}}
+    </q-toolbar-title>
+    </q-toolbar>
+  </div>
+</template>
+<script>
+import {
+  QToolbar,
+  QToolbarTitle,
+  QBtn,
+  QIcon
+} from 'quasar'
+export default {
+  props: ['title'],
+  data () {
+    return {
+      canGoBack: window.history.length > 1
+    }
+  },
+  methods: {
+    goBack () {
+      window.history.go(-1)
+    }
+  },
+  components: {
+    QToolbar,
+    QToolbarTitle,
+    QBtn,
+    QIcon
+  }
+}
+</script>
