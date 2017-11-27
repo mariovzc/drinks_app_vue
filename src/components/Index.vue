4cd490c87aee9cae2a5e022c79eed005535a644c
Mario vizcaino <mario.vizcainos@gmail.com>
2017-11-26 22:22:37 -0500

    added nav bar for the main view

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,42 +1,57 @@
 <template>
-  <div class="layout text-center">
-    <h3>Chose a Drink Type</h3>
-    <div class="btn-list">
-      <div class="btn-container">
-        <router-link :to="{path: '/drinklist/Alcoholic'}">
-          <q-btn class="bg-primary">
-            Alcoholic
-          </q-btn>
-        </router-link>
+  <div>
+    <q-toolbar slot="header">
+      <q-toolbar-title to="/" class="text-center" >
+      {{title}}
+      </q-toolbar-title>
+    </q-toolbar>
+    <div class="layout text-center">
+      <h3>Chose a Drink Type</h3>
+      <div class="btn-list">
+        <div class="btn-container">
+          <router-link :to="{path: '/drinklist/Alcoholic'}">
+            <q-btn class="bg-primary">
+              Alcoholic
+            </q-btn>
+          </router-link>
+        </div>
+        <div class="btn-container">
+          <router-link :to="{path: '/drinklist/Non_Alcoholic'}">
+            <q-btn  class="bg-secondary">
+              Non Alcoholic
+            </q-btn>
+          </router-link>
+        </div>
+        <div class="btn-container">
+          <router-link :to="{path: '/randomdrink'}">
+            <q-btn class="bg-tertiary">
+              Random
+            </q-btn>
+          </router-link>
+        </div>
       </div>
-      <div class="btn-container">
-        <router-link :to="{path: '/drinklist/Non_Alcoholic'}">
-          <q-btn  class="bg-secondary">
-            Non Alcoholic
-          </q-btn>
-        </router-link>
-      </div>
-      <div class="btn-container">
-        <router-link :to="{path: '/randomdrink'}">
-          <q-btn class="bg-tertiary">
-            Random
-          </q-btn>
-        </router-link>
-      </div>
-    </div>
+    </div>    
   </div>
 </template>
 
 <script>
-import {QBtn} from 'quasar'
+import {
+  QToolbar,
+  QToolbarTitle,
+  QBtn
+} from 'quasar'
+
 export default {
   name: 'index',
   data () {
     return {
+      title: 'Drink Master'
     }
   },
   components: {
-    QBtn
+    QBtn,
+    QToolbar,
+    QToolbarTitle
   }
 }
 </script>
